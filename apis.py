72e011e27e19e6d8a6bb04613b36db1660adff50
matiasespinola <rojasmati98@gmail.com>
2025-12-09 15:29:36 -0300

    Ruta de prueba y tokens estaticos

--- apis.py
+++ apis.py
@@ -3,3 +3,9 @@ from datetime import datetime
 import sqlite3
 
 app = Flask(__name__)
+
+@app.route('/')
+def chequeo():
+    return jsonify({"mensaje": "App activa", "status": "ok"}), 200
+
+TOKENS = {"DcOP3456nE7890", "XyT8nQpZ19JkVr7L", "mP4z6Q2y1Wl8HsEo"}
