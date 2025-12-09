e4a98b5131ba40cf017a365b3d1f5453d2b152a4
matiasespinola <rojasmati98@gmail.com>
2025-12-09 15:35:18 -0300

    Funcion para dara aletoriedad

--- pedidos.py
+++ pedidos.py
@@ -15,3 +15,29 @@ LOGGINGS = [
 ]
 
 PESOS = [20, 60, 10, 7, 3]
+
+def simular_logs():
+    cantidad = 20
+    for _ in range(cantidad):
+        logs = random.choices(LOGGINGS, weights=PESOS, k=1)[0]
+        logs_data = {
+            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3],
+            "service": logs["service"],
+            "severity": logs["severity"],
+            "message": logs["message"]
+        }
+        try:
+            respuesta = requests.post(
+                API_URL,
+                headers={"Authorization": f"Bearer {AUTHORIZATION_TOKEN}"},
+                json={"logs": [logs_data]}
+            )
+            print(f"Enviado: {logs_data} -> Respuesta: {respuesta.status_code} - {respuesta.text}")
+        except Exception as e:
+            print("Error al enviar log:", e)
+
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    simular_logs()
